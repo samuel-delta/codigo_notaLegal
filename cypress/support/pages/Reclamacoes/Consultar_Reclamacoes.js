c4feed91575b23e585506230f07e5e9d239ac7aa
Fernanda Carolina <fernanda.aguiar@deltapoint.com.br>
2025-07-02 10:32:41 -0300

    Começo do reclamacao

---
+++ cypress/support/pages/Reclamacoes/Consultar_Reclamacoes.js
@@ -0,0 +1,9 @@
+export const consultarReclamacoesElements = {
+  // MENUS E SUBMENOS
+  menuReclamacoes: 'span:contains("Reclamações")',
+  subMenuConsultarReclamacoes: 'span:contains("Consultar Reclamações")',
+
+  //INPUTS
+  inputReclamacao: '#reclamacao',
+  
+}
